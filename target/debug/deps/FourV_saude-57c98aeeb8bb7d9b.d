f9afefeb48a33fc393de042bafa454462d240259
Sandro Reis <consultorsandro@hotmail.com>
2025-06-07 16:59:21 -0300

    Doc Cargo.toml update

---
+++ target/debug/deps/FourV_saude-57c98aeeb8bb7d9b.d
@@ -0,0 +1,12 @@
+C:\APLICATIVOS\4V_SAUDE\target\debug\deps\libFourV_saude-57c98aeeb8bb7d9b.rmeta: src\main.rs src\bmi\mod.rs src\bmi\calculator.rs src\metabolism\mod.rs src\metabolism\tmb.rs src\body_fat.rs src\whr\mod.rs src\whr\calculator.rs
+
+C:\APLICATIVOS\4V_SAUDE\target\debug\deps\FourV_saude-57c98aeeb8bb7d9b.d: src\main.rs src\bmi\mod.rs src\bmi\calculator.rs src\metabolism\mod.rs src\metabolism\tmb.rs src\body_fat.rs src\whr\mod.rs src\whr\calculator.rs
+
+src\main.rs:
+src\bmi\mod.rs:
+src\bmi\calculator.rs:
+src\metabolism\mod.rs:
+src\metabolism\tmb.rs:
+src\body_fat.rs:
+src\whr\mod.rs:
+src\whr\calculator.rs:
