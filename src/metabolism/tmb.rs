8b71baddd51964a562e266388ae77ea89e94624a
Sandro Reis <consultorsandro@hotmail.com>
2025-06-11 20:21:46 -0300

    Corrected BMR formula

--- src/metabolism/tmb.rs
+++ src/metabolism/tmb.rs
@@ -17,7 +17,7 @@ pub struct TmbData {
 
 pub struct TmbCalculator; // This struct serves as a namespace for TMB calculation methods
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum TmbCategory {
     VeryLow,
     Low,
@@ -30,10 +30,10 @@ impl TmbCalculator {
     pub fn calculate(data: &TmbData) -> f32 {
         match data.gender {
             Gender::Male => {
-                66.0 + (13.7 * data.weight) + (5.0 * data.height * 100.0) - (6.8 * data.age as f32)
+                88.36 + (13.4 * data.weight) + (4.8 * data.height * 100.0) - (5.7 * data.age as f32)
             }
             Gender::Female => {
-                655.0 + (9.6 * data.weight) + (1.8 * data.height * 100.0) - (4.7 * data.age as f32)
+                447.6 + (9.2 * data.weight) + (3.1 * data.height * 100.0) - (4.3 * data.age as f32)
             }
         }
     }
@@ -75,5 +75,62 @@ impl TmbCalculator {
              Classification: {}",
             tmb, tmb_per_kg, classification
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculate_male() {
+        let data = TmbData {
+            weight: 70.0,
+            height: 1.75,
+            age: 25,
+            gender: Gender::Male,
+        };
+        let tmb = TmbCalculator::calculate(&data);
+        assert!((tmb - 1745.1).abs() < 0.1); // Valor esperado para um homem de 70kg, 1.75m e 25 anos
+    }
+
+    #[test]
+    fn test_calculate_female() {
+        let data = TmbData {
+            weight: 60.0,
+            height: 1.65,
+            age: 30,
+            gender: Gender::Female,
+        };
+        let tmb = TmbCalculator::calculate(&data);
+        assert!((tmb - 1395.0).abs() < 0.1); // Valor esperado para uma mulher de 60kg, 1.65m e 30 anos
+    }
+
+    #[test]
+    fn test_classify_male() {
+        let tmb = 1745.1; // TMB calculado para o teste
+        let weight = 70.0;
+        let category = TmbCalculator::classify(tmb, weight, &Gender::Male);
+        assert_eq!(category, TmbCategory::Normal);
+    }
+
+    #[test]
+    fn test_classify_female() {
+        let tmb = 1395.0; // TMB calculado para o teste
+        let weight = 60.0;
+        let category = TmbCalculator::classify(tmb, weight, &Gender::Female);
+        assert_eq!(category, TmbCategory::Normal);
+    }
+
+    #[test]
+    fn test_evaluation_result() {
+        let tmb = 1745.1;
+        let weight = 70.0;
+        let category = TmbCategory::Normal;
+        let result = TmbCalculator::evaluation_result(tmb, weight, &category);
+        let expected = "Your Basal Metabolic Rate (TMB) is 1745.10 kcal/day.\n\
+                        Your TMB per kg is 24.93 kcal/kg/day.\n\
+                        Classification: Normal";
+        assert_eq!(result, expected);
     }
 }
